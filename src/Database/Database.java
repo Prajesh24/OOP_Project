649585dc40c4cec3ba71d2b86137341d326a90f6
Prajesh24 <149761502+Prajesh24@users.noreply.github.com>
2024-08-15 13:59:08 +0545

    adding database

--- src/Database/Database.java
+++ src/Database/Database.java
@@ -4,10 +4,11 @@
  */
 package Database;
 
-/**
- *
- * @author prajesh
- */
-public class Database {
-    
+import java.sql. *;
+  
+public interface Database {
+    Connection openConnection(); // Propagate exception for callers to handle.
+    void closeConnection (Connection conn); // Propagate exception.
+    ResultSet runQuery (Connection conn, String query); // Propagate exception.
+    int executeUpdate (Connection conn, String query) ; // Propagate exception.
 }
